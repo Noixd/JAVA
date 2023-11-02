e517f0436e1828d3ee2310c88fffba046e823560
Noix <noiodox@mail.ru>
2023-11-02 11:06:20 +0300

    Add task1 from lab3

---
+++ Lab3/Task1.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+public class Task1 {
+    
+    public static void main(String args[]) {
+
+        String path = "АБвrде 123ЖзИ";
+        char[] word_inp = path.toCharArray();
+        char[] word_out = new char[word_inp.length];
+        int j = 0;
+
+        for(int i = 0; i <= word_out.length-1; i++) {
+            
+            if (Character.isUpperCase(word_inp[i])) word_out[j++] = word_inp[i];
+        }
+        
+        System.out.println(word_out);
+    }
+}
